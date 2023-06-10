226cb5426716ee5e741ef55ee1d16f256924b1f2
rezaul118255 <rezaul118255@gmail.com>
2023-06-11 02:54:19 +0600

    admin are selected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,22 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -33,7 +50,25 @@ async function run() {
         const ClassCollection = client.db("RestuDb").collection("Class");
         const InstructorsCollection = client.db("RestuDb").collection("Instructors");
         const CartCollection = client.db("RestuDb").collection("Carts");
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+
+            res.send({ token })
+        })
+
+
+
+
         // user related apis 
+
+        app.get('/users', async (req, res) => {
+            const result = await UsersCollection.find().toArray();
+            res.send(result);
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
 
@@ -47,6 +82,50 @@ async function run() {
 
             const result = await UsersCollection.insertOne(user);
             res.send(result);
+        })
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await UsersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
+
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+
+            const result = await UsersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+
+        })
+        app.patch('/users/Instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'Instructor'
+                },
+            };
+
+            const result = await UsersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+
         })
 
 
@@ -69,11 +148,15 @@ async function run() {
 
         // cart collection
 
-        app.get('/Carts', async (req, res) => {
+        app.get('/Carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
 
             if (!email) {
                 res.send([]);
+            }
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'porviden access' })
             }
             const query = { email: email };
             const result = await CartCollection.find(query).toArray();
